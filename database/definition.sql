4be1a7f52a27af23baabe160d55fb2f3d6862946
Oscar Aguinagalde <oscaraguinagalde@gmail.com>
2022-10-31 15:59:40 +0900

    define sqlite tables

--- database/definition.sql
+++
@@ -1,2 +0,0 @@
-create table students (id int, name varchar(255), age int, grade int);
-insert into students values(1, 'John', 6, 1);
--- database/setup_database.ts
+++ database/setup_database.ts
@@ -1,8 +1,6 @@
 import { Database } from 'bun:sqlite';
 import { readFile } from 'node:fs/promises';
   
-async function setup_database() {
-    const file_content = await readFile('./definition.sql', { encoding: "utf8" });
-    const db = new Database("data.db");
-    db.run(file_content);
-}
+const file_content = await readFile('./sql/definition.sql', { encoding: "utf8" });
+const db = new Database("data.db");
+console.log(db.run(file_content));
