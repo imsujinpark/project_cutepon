fff38dc03910a07e50f54a48442126a213b2334d
imsujinpark <imsujinpark@gmail.com>
2022-11-07 23:07:08 +0900

    feat: routes added in app.js

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,21 @@
-import './App.css'
-import GlobalStyle from './GlobalStyle'
-import Login from './pages/Login'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './components/layout/Nav';
+import GlobalStyle from './GlobalStyle';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
-function App() {
+const App: React.FunctionComponent = () => {
     return (
-        <div>
+        <BrowserRouter>
             <GlobalStyle />
-            <Login />
-        </div>
-    )
-}
+            <Nav />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
-export default App
+export default App;
