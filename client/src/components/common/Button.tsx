1e1e83e02e9cc7aa5ee62ba59776946d91c98e4f
imsujinpark <imsujinpark@gmail.com>
2022-11-08 01:32:17 +0900

    chore: general button component created

--- client/src/components/common/Button.tsx
+++ client/src/components/common/Button.tsx
@@ -1,9 +1,37 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-const Button = () => {
-    return <StyledButton></StyledButton>
-}
+type Class = {
+    className?: string;
+    content: string;
+    onClick?: { (): void };
+};
 
-const StyledButton = styled.button``
+const Button = ({ className, content, onClick }: Class) => {
+    return (
+        <StyledButton className={className} onClick={onClick}>
+            {content}
+        </StyledButton>
+    );
+};
 
-export default Button
+const StyledButton = styled.button`
+    width: 100px;
+    height: 48px;
+    border-radius: 8px;
+    font-weight: bold;
+    font-size: 12px;
+    &.grey {
+        background-color: var(--liver-200);
+        color: var(--liver-600);
+    }
+    &.lightpink {
+        background-color: var(--lightpink-400);
+        color: var(--liver-600);
+    }
+    &.primary {
+        background-color: var(--primary-500);
+        color: var(--white);
+    }
+`;
+
+export default Button;
