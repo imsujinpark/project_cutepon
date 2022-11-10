7a0d31abe95deb8aba117d92c40ab01d482b8ac0
imsujinpark <imsujinpark@gmail.com>
2022-11-10 18:49:36 +0900

    feat: redux toolkit, persist, thunk setup

---
+++ client/src/store.ts
@@ -0,0 +1,29 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+//for redux persist
+import { persistReducer } from "redux-persist";
+import thunk from "redux-thunk";
+import storageSession from "redux-persist/lib/storage/session";
+import userSlice from "./features/userSlice";
+
+const rootReducer = combineReducers({
+    user: userSlice.reducer,
+});
+
+const persistConfig = {
+    key: "root",
+    storage: storageSession, // will be saved in session storage
+    whitelist: ["user"], // will save only user reducer in session storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    devTools: process.env.NODE_ENV !== "production",
+    middleware: [thunk],
+});
+
+export default store;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
