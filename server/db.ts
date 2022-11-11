be22b9f745b598d1dc902b4088d45228323fe386
Oscar Aguinagalde <oscaraguinagalde@gmail.com>
2022-11-11 12:17:17 +0900

    oops

--- server/db.ts
+++ server/db.ts
@@ -2,12 +2,16 @@ import { User } from './src/User.js';
 import { Coupon } from './src/Coupon.js';
 import { Database, Statement } from './src/sqlite-async.js';
 
-const database:Database = await Database.open("./data/database.sqlite3");
-database.on("error", (err: Error) => {
-    throw err;
-});
+const main = async () => {
+    const database: Database = await Database.open("./data/database.sqlite3");
+    database.on("error", (err: Error) => {
+        throw err;
+    });
+    
+    await User.reset_table(database);
+    await Coupon.reset_table(database);
+    await User.initialize_statements(database);
+    await Coupon.initialize_statements(database);
+}
 
-await User.reset_table(database);
-await Coupon.reset_table(database);
-await User.initialize_statements(database);
-await Coupon.initialize_statements(database);
+main();
