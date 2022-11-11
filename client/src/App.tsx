68e7100122d111ac119ff8155da0fd7590453a7d
imsujinpark <imsujinpark@gmail.com>
2022-11-11 15:58:04 +0900

    fix: prevent elements rendering before authorization header is set

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import styled from 'styled-components';
 import Nav from './components/layout/Nav';
@@ -10,23 +10,49 @@ import OAuth2RedirectHandler from './pages/OAuth2RedirectHandler';
 import ReceivedCoupons from './pages/ReceivedCoupons';
 import SentCoupons from './pages/SentCoupons';
 
+import axios from 'axios';
+
+import { useSelector } from 'react-redux';
+import { RootState } from './store';
+
 const App: React.FunctionComponent = () => {
+    const [isAuthChecked, setIsAuthChecked] = useState<boolean>(false);
+    console.log({ isAuthChecked });
+
+    // login status
+    const { isLoggedIn, token, refreshToken } = useSelector(
+        (state: RootState) => {
+            return state.user;
+        }
+    );
+
+    console.log({ isLoggedIn, token, refreshToken });
+
+    // useEffect(() => {
+    //     if (isLoggedIn) {
+    //         axios.defaults.headers.common['Authorization'] = token;
+    //     }
+    //     setIsAuthChecked(true);
+    // }, []);
+
     return (
         <BrowserRouter>
             <GlobalStyle />
             <Div>
                 <Nav />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route
-                        path="/oauth2/tokens"
-                        element={<OAuth2RedirectHandler />}
-                    />
-                    <Route path="/received" element={<ReceivedCoupons />} />
-                    <Route path="/sent" element={<SentCoupons />} />
-                    <Route path="/new" element={<NewCoupon />} />
-                </Routes>
+                {isAuthChecked && (
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route
+                            path="/oauth2/tokens"
+                            element={<OAuth2RedirectHandler />}
+                        />
+                        <Route path="/received" element={<ReceivedCoupons />} />
+                        <Route path="/sent" element={<SentCoupons />} />
+                        <Route path="/new" element={<NewCoupon />} />
+                    </Routes>
+                )}
             </Div>
         </BrowserRouter>
     );
