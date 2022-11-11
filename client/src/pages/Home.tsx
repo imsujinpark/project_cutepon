d67146bc0a36f6d0c8dceda6bb1c57d7da52859d
imsujinpark <imsujinpark@gmail.com>
2022-11-12 02:38:20 +0900

    feat: silent refresh and logout tested

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,41 @@
 import styled from 'styled-components';
+// redux related
+import { useDispatch, useSelector } from 'react-redux';
+import { logoutFulfilled } from '../features/userSlice';
+import { RootState } from '../store';
+import { persistor } from '../index';
+import axios from 'axios';
+import { silentRefresh } from '../common/utils';
 
 const Home = () => {
+    const dispatch = useDispatch();
+
+    // purge function to remove state from session storage
+    const purge = async () => {
+        await persistor.purge();
+    };
+
+    const logout = () => {
+        dispatch(logoutFulfilled());
+        setTimeout(() => purge(), 2000);
+        window.location.reload();
+    };
+
+    const hello = async () => {
+        try {
+            const { data } = await axios.get(`/api/hello`);
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
         <Container>
             <div>Home</div>
+            <button onClick={logout}>Logout</button>
+            <button onClick={hello}>Hello</button>
+            <button onClick={() => silentRefresh('111')}>REFRESH</button>
         </Container>
     );
 };
