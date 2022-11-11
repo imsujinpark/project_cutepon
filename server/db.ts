d98a9da0442b2c5eb6be3ebb52340b42c1afd2fa
Oscar Aguinagalde <oscaraguinagalde@gmail.com>
2022-11-11 12:11:13 +0900

    Add script for databas manipulation: Migrations, modifications, testing...

---
+++ server/db.ts
@@ -0,0 +1,13 @@
+import { User } from './src/User.js';
+import { Coupon } from './src/Coupon.js';
+import { Database, Statement } from './src/sqlite-async.js';
+
+const database:Database = await Database.open("./data/database.sqlite3");
+database.on("error", (err: Error) => {
+    throw err;
+});
+
+await User.reset_table(database);
+await Coupon.reset_table(database);
+await User.initialize_statements(database);
+await Coupon.initialize_statements(database);
