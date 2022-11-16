97848a771cdc58443d9af45916343584a5830a7f
Oscar Aguinagalde <oscaraguinagalde@gmail.com>
2022-11-17 00:21:49 +0900

    script to update db to version 2

--- server/db.ts
+++ server/db.ts
@@ -1,17 +1,27 @@
 import { User } from './src/User.js';
 import { Coupon } from './src/Coupon.js';
 import { Database, Statement } from './src/sqlite-async.js';
+import { UserCoupon } from './src/UserCoupon.js';
 
+const version = 2;
 const main = async () => {
     const database: Database = await Database.open("./data/database.sqlite3");
     database.on("error", (err: Error) => {
         throw err;
     });
     
-    await User.reset_table(database);
-    await Coupon.reset_table(database);
-    await User.initialize_statements(database);
-    await Coupon.initialize_statements(database);
+    if (version <= 1) {
+        await User.reset_table(database);
+        await Coupon.reset_table(database);
+     
+        await User.initialize_statements(database);
+        await Coupon.initialize_statements(database);
+    }
+
+    if (version <= 2) {
+        await UserCoupon.reset_table(database);
+        await UserCoupon.initialize_statements(database);
+    }
 }
 
 main();
