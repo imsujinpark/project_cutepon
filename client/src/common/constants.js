ab56145f897933450e9f13c6731762e60979f06a
imsujinpark <imsujinpark@gmail.com>
2023-04-11 00:46:38 +0900

    toUpperCase typo fixed

--- client/src/common/constants.js
+++ client/src/common/constants.js
@@ -1,9 +1,9 @@
 export const Status = {
-    ACTIVE: "active",
-    DISABLED: "disabled"
-}
+	ACTIVE: "active",
+	DISABLED: "disabled"
+};
 
 export const StatusText = {
-    ACTIVE: Status.ACTIVE.charAt(0).toUpperCase + Status.ACTIVE.slice(1),
-    DISABLED: Status.DISABLED.charAt(0).toUpperCase + Status.DISABLED.slice(1),
-}
+	ACTIVE: Status.ACTIVE.charAt(0).toUpperCase() + Status.ACTIVE.slice(1),
+	DISABLED: Status.DISABLED.charAt(0).toUpperCase() + Status.DISABLED.slice(1),
+};
