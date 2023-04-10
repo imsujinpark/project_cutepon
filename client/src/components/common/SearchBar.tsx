c4fa9a819f0a9317590ef4d163316011b6331229
imsujinpark <imsujinpark@gmail.com>
2023-04-11 01:19:10 +0900

    clear search keyword added

--- client/src/components/common/SearchBar.tsx
+++ client/src/components/common/SearchBar.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+// font-awesome icon
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 interface SearchBarProps {
     keyword: string;
@@ -11,20 +14,51 @@ const SearchBar = ({ keyword, setKeyword }: SearchBarProps) => {
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setKeyword(e.target.value);
 	};
+
+	const clearKeyword = () => {
+		setKeyword("");
+	};
     
 	return (
-		<div>
+		<Container>
 			<Input value={keyword} onChange={handleChange} />
-		</div>
+			<StyledFontAwesomeIcon icon={faXmark} onClick={clearKeyword}/>
+		</Container>
 	);
 };
 
-const Input = styled.input`
+const Container = styled.div`
     width: 348px;
     height: 32px;
     border: 3px solid var(--primary-400);
-    border-radius: 4px;
-    margin-bottom: 16px;
+	border-radius: 4px;
+	margin-bottom: 16px;
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+const Input = styled.input`
+    width: 320px;
+	border: none;
+	margin: 0 4px;
+	&:focus{
+    	outline: none;
+}
+`;
+
+const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
+	color: var(--primary-400);
+	margin-right: 8px;
+	cursor: pointer;
+	&:hover {
+		color: var(--primary-500);
+	}
+	&:active {
+		color: var(--primary-500);
+		transform: translate(1px, 1px);
+	}
 `;
 
 export default SearchBar;
